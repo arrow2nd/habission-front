d3f6df2162215106c570586c86fef129f939dbb7
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-08-31 00:34:25 +0900

    chore: fresh.gen.ts

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,6 +6,9 @@ import * as $0 from "./routes/_app.tsx";
 import * as $1 from "./routes/index.tsx";
 import * as $2 from "./routes/mypage.tsx";
 import * as $$0 from "./islands/LoginForm.tsx";
+import * as $$1 from "./islands/logout.tsx";
+import * as $$2 from "./islands/mission.tsx";
+import * as $$3 from "./islands/user.tsx";
 
 const manifest = {
   routes: {
@@ -15,9 +18,9 @@ const manifest = {
   },
   islands: {
     "./islands/LoginForm.tsx": $$0,
-    "./islands/logout.tsx": $$0,
-    "./islands/mission.tsx": $$1,
-    "./islands/user.tsx": $$2,
+    "./islands/logout.tsx": $$1,
+    "./islands/mission.tsx": $$2,
+    "./islands/user.tsx": $$3,
   },
   baseUrl: import.meta.url,
 };
