d35c9a12a1fb242065fe94f63a5b914ba607e09b
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-08-30 23:22:07 +0900

    feat(islands/mission): チェックボックスを追加

--- islands/mission.tsx
+++ islands/mission.tsx
@@ -1,12 +1,36 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
+
 import Card from "@components/card.tsx";
+import { Mission } from "@interfaces/missions.ts";
 
 export default function MissionCard() {
+  const missions: Mission[] = [
+    {
+      title: "テスト",
+      desc: "説明",
+      isDone: false,
+    },
+    {
+      title: "テスト",
+      desc: "説明",
+      isDone: false,
+    },
+  ];
+
+  // MEMO: idほしいかも
+
   return (
     <Card title="Today's Mission" icon="checklist">
-      test
+      {missions.map(({ title, desc, isDone }) => (
+        <div>
+          <input type="checkbox" id="scales" name="scales" checked={isDone} />
+          <label class={tw`ml-1`} for="scales" title={desc}>
+            {title}
+          </label>
+        </div>
+      ))}
     </Card>
   );
 }
