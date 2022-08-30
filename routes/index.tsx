aa44f4d1e20a1beaa2d9bea64e314b52069175d2
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-08-31 02:26:52 +0900

    feat:

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,8 @@
 /** @jsx h */
 import { h } from "preact";
-import { tw } from "@twind";
-import LoginForm from "../islands/LoginForm.tsx";
+import { tw } from "@utils/twind.ts";
+
+import LoginForm from "@islands/LoginForm.tsx";
 
 export default function Home() {
   return (
