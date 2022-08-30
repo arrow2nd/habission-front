fb3ae30329adb69fc6af7cd510f03f78d7d72420
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-08-30 18:56:53 +0900

    feat(routes): フォントとアイコンを設定

---
+++ routes/_app.tsx
@@ -0,0 +1,36 @@
+/** @jsx h */
+/** @jsxFrag Fragment */
+import { Fragment, h } from "preact";
+import { tw } from "@twind";
+import { Head, asset } from "$fresh/runtime.ts";
+import { AppProps } from "$fresh/server.ts";
+
+export default function App(props: AppProps) {
+  const title = "maningen-mission";
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta property="og:title" content={title} />
+        <meta name="twitter:title" content={title} />
+        <link
+          rel="stylesheet"
+          href={asset(
+            "https://api.fonts.coollabs.io/css2?family=M+PLUS+Rounded+1c:wght@400;500&display=swap"
+          )}
+        />
+        <link
+          rel="stylesheet"
+          href={asset(
+            "https://cdn.jsdelivr.net/npm/@tabler/icons@1.76.0/iconfont/tabler-icons.min.css"
+          )}
+        />
+        <link rel="stylesheet" href={asset("./globals.css")} />
+      </Head>
+      <div class={tw`text-text bg-background font-default`}>
+        <props.Component />
+      </div>
+    </>
+  );
+}
